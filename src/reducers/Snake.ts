10d632296753b030356b82a05abfeb2038b08658
Kyle Close <k.james.close@gmail.com>
2023-07-03 20:27:47 -0400

    progress

--- src/reducers/Snake.ts
+++ src/reducers/Snake.ts
@@ -28,16 +28,35 @@ const SnakeSlice = createSlice({
 		},
         addSnakeBodySegment(state, action: PayloadAction<{
             bodyCoordinates: [number, number]
-        }>){
+        }>) {
 			const snake = state.snake
             const snakeBody = {
                 current: action.payload.bodyCoordinates,
                 next: snake[snake.length - 1].current
             }
             snake.push(snakeBody)
+        },
+        moveSnakeOnUpdate(state, action: PayloadAction<{
+            direction: Direction
+        }>) {
+            const snake = state.snake
+            const snakeLength = snake.length
+
+            for(let i = 0; i < snakeLength; i++){
+                if(i === 0){
+                    // On head
+                    snake[i].current = snake[i].next;
+                    snake[i].next = getNextHeadCoordinates(action.payload.direction, snake[i].current)
+                }else{
+                    // On body
+                    snake[i].current = snake[i].next
+                    snake[i].next = snake[i - 1].current
+                }
+            }
         }
 	},
 });
 
+
 export default SnakeSlice.reducer;
-export const { initSnakeHead, addSnakeBodySegment } = SnakeSlice.actions;
+export const { initSnakeHead, addSnakeBodySegment, moveSnakeOnUpdate } = SnakeSlice.actions;
--- src/utils/OnUpdate.ts
+++ src/utils/OnUpdate.ts
@@ -1,7 +1,14 @@
 import { Direction } from "../reducers/Direction";
+import store from '../reducers'
+import { moveSnakeOnUpdate } from "../reducers/Snake";
 
 export function update(){
-    console.log('updating game...')
+    // Check if snake head hit wall
+
+    // Check if snake ate food
+
+    // Update snakes position based on next positions
+    updateSnakePosition()
 }
 
 export function getNextHeadCoordinates(direction: Direction, currentCoordinates:[number, number]): [number, number]{
@@ -11,4 +18,11 @@ export function getNextHeadCoordinates(direction: Direction, currentCoordinates:
     else if(direction === Direction.WEST) x -= 1
     else if(direction === Direction.EAST) x += 1
     return [x, y]
+}
+
+function updateSnakePosition(){
+    const state = store.getState()
+    const direction = state.Direction.direction
+
+    store.dispatch(moveSnakeOnUpdate({direction: direction}))
 }
